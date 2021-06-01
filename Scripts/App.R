35d99b3082a26f7e4460af29f9be9ddceaad956c
Louis Condevaux <condevaux@chapman.edu>
2021-06-01 15:35:33 -0700

    "Working on server, using slider as a reactive input to change the number of users in the dataframe"

--- Scripts/App.R
+++ Scripts/App.R
@@ -1,8 +1,29 @@
-source(titkok_trending.R)
-library(shiny)
+
+source("main.R")
 
 
 
-ui <- fluidPage() 
-server <- function(input, output){} 
+
+ui <- fluidPage(
+  sliderInput("users", min = 1, max = 10000, value = 2, 
+              label = "choose the number of users in the sampler" ),
+  plotlyOutput("graph")
+)
+
+server <- function(input, output){
+
+  #df$id <- seq(1, nrow(df))
+  
+  output$graph <- renderPlotly({
+    slider <- input$users
+    df = paste0("SELECT user_name, n_shares, n_plays, n_likes, n_comments, video_length, id AS COUNT(user_name)
+      FROM trending_vids
+      WHERE n_likes >", slider , " GROUP BY user_name
+      ORDER BY n_likes")
+    df <- sqldf(eq)
+    #df <- reactive(df)
+    shiny_reg(df, df$n_likes, df$n_plays, df$n_shares, df$user_name)
+    
+  })
+} 
 shinyApp(ui = ui, server = server)
--- Scripts/main.R
+++ Scripts/main.R
@@ -1,17 +1,26 @@
 setwd("/Users/Louis/Desktop/Projects/TikTok/Scripts")
+
 source("functions.R")
 
 
-trending_vids <- read.csv("trending_vids.csv")
+trending_vids <- read.csv("Datasets/trending_vids.csv")
 #View(trending_vids)
 
 #DATASET1 with top 33 users
+n <- 33
+eq = paste0("SELECT user_name, n_shares, n_plays, n_likes, n_comments, video_length
+      FROM trending_vids
+      WHERE n_likes >", n, " GROUP BY user_name
+      ORDER BY n_likes")
+
+new_tik <- sqldf(eq)
 
 tiktok <- sqldf('SELECT user_name, n_shares, n_plays, n_likes, n_comments, video_length
       FROM trending_vids
       WHERE n_likes > 10000000
       GROUP BY user_name
       ORDER BY n_likes')
+tiktok$group <- c( rep('Q1', 8), rep('Q2', 8), rep('Q3', 8), rep('Q4', 9))
 
 #DATASET2 with top 5000 users
 tiktok2 <- sqldf('SELECT user_name, n_shares, n_plays, n_likes, n_comments, video_length
@@ -28,7 +37,7 @@ data$groupnum <- c( rep('0', 8), rep('25', 8), rep('50', 8), rep('75', 9))
 
 #Circle barplot
 
-Shiny_Circle_Bar(tiktok)
+#Shiny_Circle_Bar(tiktok)
 
 #ggsave("Viz/CirclePlot.png", p, bg = "transparent")
 
@@ -42,7 +51,7 @@ Shiny_Circle_Bar(tiktok)
 
 #DATASET only for words
 
-shiny_Wordcloud(tiktok_words,color = 'random-light',backgroundColor = "black" )
+#shiny_Wordcloud(tiktok_words,color = 'random-light',backgroundColor = "black" )
 
 #Saving the wordcloud
 #saveWidget(word_viz, file="Viz/word_viz.html", selfcontained = F,  background = "transparent")
@@ -56,10 +65,10 @@ shiny_Wordcloud(tiktok_words,color = 'random-light',backgroundColor = "black" )
 
 
 top33_reg <- shiny_reg(data, data$n_likes, data$n_plays, data$n_shares, data$user_name)
-top33_reg
+#top33_reg
 #Plotting normally and interactively
 top5000_reg <- shiny_reg(tiktok2, tiktok2$n_likes, tiktok2$n_plays, tiktok2$n_shares, tiktok2$user_name)
-top5000_reg
+#top5000_reg
 
 #ggsave("Viz/scatterplot33.png", s1,  bg = "transparent")
 #htmlwidgets::saveWidget(ggplotly(s1), "Viz/scatterplot33.html")
@@ -73,7 +82,7 @@ top5000_reg
 ################################################
 #CORR/DENSITY
 
-ggcorr(tiktok)
-shiny_corr(data, data$n_likes, data$group)
+#ggcorr(tiktok)
+#shiny_corr(data, data$n_likes, data$group)
 
 
